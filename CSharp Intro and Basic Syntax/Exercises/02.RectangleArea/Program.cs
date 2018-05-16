c622e313a81dd3c020c7cf01aa92d46938fa52fe
Kalina M <milanova1203@gmail.com>
2018-05-16 18:12:21 +0300

    Add files via upload

---
+++ CSharp Intro and Basic Syntax/Exercises/02.RectangleArea/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _02.RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double sum = a * b;
+            Console.WriteLine($"{sum:f2}");
+        }
+    }
+}
---
+++ CSharp Intro and Basic Syntax/Exercises/03.MilesToKm/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _03.MilesToKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double miles = double.Parse(Console.ReadLine());
+            double milesToKm = miles * 1.60934;
+            Console.WriteLine($"{milesToKm:f2}");
+        }
+    }
+}
