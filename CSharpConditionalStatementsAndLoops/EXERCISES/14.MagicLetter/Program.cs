b9e462d7e537d23556a976b2434e8a0d7bbc2523
Kalina M <milanova1203@gmail.com>
2018-05-19 01:49:21 +0300

    Add files via upload

---
+++ CSharpConditionalStatementsAndLoops/EXERCISES/14.MagicLetter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.MagicLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstLetter = char.Parse (Console.ReadLine());
+            char secondLetter = char.Parse(Console.ReadLine());
+            char noPrintLetter = char.Parse(Console.ReadLine());
+
+            for (char a = firstLetter; a <= secondLetter; a++)
+            {
+                for (char b = firstLetter; b <= secondLetter; b++)
+                {
+                    for (char c = firstLetter; c <= secondLetter; c++)
+                    {
+                        if(a != noPrintLetter  && b != noPrintLetter && c != noPrintLetter)
+                        {
+                            Console.Write($"{a}{b}{c} ");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CSharpConditionalStatementsAndLoops/EXERCISES/15.NeighbourWars/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.NeighbourWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int peshoDamage = int.Parse(Console.ReadLine());
+            int goshoDamage = int.Parse(Console.ReadLine());
+
+            int peshoHealth = 100;
+            int goshoHealth = 100;
+            int br = 0;
+
+            while (true)
+            {
+                br += 1;
+                if (br % 2 == 0 && br != 1)
+                {
+                    peshoHealth -= goshoDamage;
+                    if (peshoHealth <= 0)
+                    {
+
+                        Console.WriteLine($"Gosho won in {br}th round.");
+                        return;
+                    }
+                    Console.WriteLine($"Gosho used Thunderous fist and reduced Pesho to {peshoHealth} health.");
+                }
+                else
+                {
+                    goshoHealth -= peshoDamage;
+                    if (goshoHealth <= 0)
+                    {
+
+                        Console.WriteLine($"Pesho won in {br}th round.");
+                        return;
+                    }
+                    Console.WriteLine($"Pesho used Roundhouse kick and reduced Gosho to {goshoHealth} health.");
+                }
+                if (br % 3 == 0)
+                {
+                    peshoHealth += 10;
+                    goshoHealth += 10;
+                }
+
+            }
+        }
+    }
+}
