24d39a2c6cd7a0ed62ecacec200ee7bfbbc649b8
Kalina M <milanova1203@gmail.com>
2018-05-21 19:21:58 +0300

    Add files via upload

---
+++ Methods. Debugging and Troubleshooting Code/LAB/03.PrintingTriangle/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.PrintingTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintTriangle(n);
+  
+        }
+        static void PrintTriangle(int n)
+        {
+            PrintTopPart(n);
+            PrintBottomPart(n);
+        }
+        static void PrintTopPart(int n)
+        {
+            for (int row = 1; row <= n; row++)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write(col + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void PrintBottomPart(int n)
+        {
+            for (int row = n - 1; row >= 1; row--)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    Console.Write(col + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
