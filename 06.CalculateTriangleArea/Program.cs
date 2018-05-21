325860e7cf9e8378cb976d8c4ad224baffae3b4c
Kalina M <milanova1203@gmail.com>
2018-05-21 19:23:37 +0300

    Add files via upload

---
+++ 06.CalculateTriangleArea/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.CalculateTriangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double w = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            double area = GetTriangleArea(w, h);
+            Console.WriteLine(area);
+        }
+        static double  GetTriangleArea(double w, double h)
+        {
+            return w * h / 2;
+        }
+    }
+}
