431596c13fb465bfa11cc64a2f2b66a198764981
Kalina M <milanova1203@gmail.com>
2018-06-01 22:37:50 +0300

    Add files via upload

---
+++ Arrays/Lab/08.CondenseArrayToNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.CondenseArrayToNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            if (numbers.Length == 1)
+            {
+                Console.WriteLine($"{numbers[0]} is already condensed to number");
+                return;
+            }
+
+            int[] condensed = new int[numbers.Length];
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                condensed[i] = numbers[i] + numbers[i + 1];
+                sum += condensed[i];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Arrays/Lab/09.ExtractMiddle12Or3Elements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _09.ExtractMiddle12Or3Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            int evenMiddle = (numbers.Length / 2) - 1;
+            int evenMiddleEnd = (numbers.Length / 2) + 1;
+
+            if (numbers.Length == 1)
+            {
+                Console.WriteLine($"{{ {numbers[0]} }}");
+            }
+            else if (numbers.Length % 2 == 0)
+            {
+
+                Console.WriteLine($"{{ {numbers[evenMiddle]}, {numbers[evenMiddle + 1]} }}");
+            }
+            else
+            {
+                Console.WriteLine($"{{ {numbers[evenMiddle]}, {numbers[evenMiddle + 1]}, {numbers[evenMiddle + 2]} }}");
+            }
+
+        }
+    }
+}
