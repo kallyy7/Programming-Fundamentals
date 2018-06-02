3128b77338f778b11b8075340b097b0e6fbb9c09
Kalina M <milanova1203@gmail.com>
2018-06-02 18:48:23 +0300

    Add files via upload

---
+++ Arrays/Exercises/06.MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.MaxSequenceOfEqualElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int br = 0;
+            int count = 0;
+            int max = 0;
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] == arr[i + 1])
+                {
+                    count++;
+                    if (count > max)
+                    {
+                        br = i - count;
+                        max = count;
+                    }
+                }
+                else
+                {
+                    count = 0;
+                }
+            }
+
+            for (int i = br + 1; i <= br + max + 1; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ Arrays/Exercises/10.PairsByDifference/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+
+namespace _10.PairsByDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int difference = int.Parse(Console.ReadLine());
+
+            PairsByDifference(numbers, difference);
+        }
+        static void PairsByDifference(int[] numbers, int difference)
+        {
+            int count = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                for (int j = 0; j < numbers.Length; j++)
+                {
+
+                    if ((numbers[i] - numbers[j]) == difference)
+                    {
+                        //Console.WriteLine($"{numbers[i]}, {numbers[j]}");
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
