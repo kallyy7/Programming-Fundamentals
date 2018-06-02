0f5ba2cb0f9cd2e3ec7a880e6ad14ef81793d101
Kalina M <milanova1203@gmail.com>
2018-06-02 18:57:16 +0300

    Add files via upload

---
+++ Arrays/Exercises/02.RotateAndSumSecond/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.RotateAndSumSecond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numberArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int rotation = int.Parse(Console.ReadLine());
+
+            int[] sumOfArrays = new int[numberArray.Length];
+
+            for (int i = 0; i < rotation; i++)
+            {
+                RotateArray(numberArray);
+
+                for (int j = 0; j < sumOfArrays.Length; j++)
+                {
+                    sumOfArrays[j] += numberArray[j];
+                }
+            }
+            Console.WriteLine(string.Join(" ", sumOfArrays));
+        }
+
+        private static void RotateArray(int[] numberArray)
+        {
+            int lastElement = numberArray[numberArray.Length - 1];
+            for (int j = numberArray.Length - 1; j > 0; j--)
+            {
+                numberArray[j] = numberArray[j - 1];
+            }
+            numberArray[0] = lastElement;
+        }
+    }
+}
+
+
