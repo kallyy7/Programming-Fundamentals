883fc3c5df4f28ce695526847956b370d9726d72
Kalina M <milanova1203@gmail.com>
2018-06-03 15:24:41 +0300

    Add files via upload

---
+++ Arrays/More Exercises/04.GrabAndGo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.GrabAndGo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] numbers = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            long searchNumber = long.Parse(Console.ReadLine());
+            int br = 0;
+            long sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                if (numbers[i] == searchNumber)
+                {
+                    br++;
+                    if(br == 2)
+                    {
+                        break;
+                    }
+                    sum += numbers[i];
+                }
+                else
+                {
+                    sum += numbers[i];
+                }
+
+            }
+
+            if (br == 2)
+            {
+                Console.WriteLine("No occurrences were found!");
+            }
+            else
+            {
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
