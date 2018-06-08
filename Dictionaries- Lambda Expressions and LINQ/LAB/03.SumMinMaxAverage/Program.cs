e251c71ab9188da6a6eaafefcef00812b253628c
Kalina M <milanova1203@gmail.com>
2018-06-08 15:29:49 +0300

    Add files via upload

---
+++ Dictionaries- Lambda Expressions and LINQ/LAB/03.SumMinMaxAverage/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.SumMinMaxAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double[] numbers = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+                numbers[i] = input;
+            }
+
+            Console.WriteLine($"Sum = {numbers.Sum()}");
+            Console.WriteLine($"Min = {numbers.Min()}");
+            Console.WriteLine($"Max = {numbers.Max()}");
+            Console.WriteLine($"Average = {numbers.Average()}");
+        }
+    }
+}
---
+++ Dictionaries- Lambda Expressions and LINQ/LAB/04.Largest3Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Largest3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            var result = numbers.OrderByDescending(x => x).Take(3);
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
