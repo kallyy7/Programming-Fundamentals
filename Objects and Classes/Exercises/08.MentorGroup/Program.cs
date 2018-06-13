29bb239b230a6e88e55ae1d44438889b6a437a45
Kalina M <milanova1203@gmail.com>
2018-06-13 17:44:50 +0300

    Add files via upload

---
+++ Objects and Classes/Exercises/08.MentorGroup/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+public class MentorGroup
+{
+    public static void Main()
+    {
+        var students = new List<Student>();
+
+        var inputLine = Console.ReadLine();
+
+        while (!inputLine.Equals("end of dates"))
+        {
+            var studentsActivities = inputLine.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+              .ToArray();
+
+            var studentName = studentsActivities[0];
+            var dates = studentsActivities
+                .Skip(1)
+                .Select(x => DateTime.ParseExact(x, "dd/MM/yyyy", CultureInfo.InvariantCulture))
+                .ToList();
+
+            if (students.Any(x => x.Name == studentName))
+            {
+                var existingStudent = students.Where(x => x.Name == studentName).First();
+                existingStudent.DatesActivities.AddRange(dates);
+            }
+            else
+            {
+                var student = new Student(studentName, new List<string>(), dates);
+                students.Add(student);
+            }
+
+            inputLine = Console.ReadLine();
+        }
+
+        inputLine = Console.ReadLine();
+
+        while (!inputLine.Equals("end of comments"))
+        {
+            var studentComment = inputLine.Split('-');
+            var studentName = studentComment[0];
+            var comment = studentComment[1];
+
+            if (students.Any(x => x.Name == studentName))
+            {
+                var existingStudent = students.Where(x => x.Name == studentName).First();
+                existingStudent.Comments.Add(comment);
+            }
+
+            inputLine = Console.ReadLine();
+        }
+
+        students = students
+            .OrderBy(x => x.Name)
+            .ToList();
+
+        foreach (var student in students)
+        {
+            Console.WriteLine(student.Name);
+            Console.WriteLine("Comments:");
+            foreach (var comment in student.Comments)
+            {
+                Console.WriteLine($"- {comment}");
+            }
+
+            Console.WriteLine("Dates attended:");
+            foreach (var date in student.DatesActivities.OrderBy(x => x))
+            {
+                Console.WriteLine($"-- {date.ToString("dd/MM/yyyy")}");
+            }
+        }
+    }
+    public class Student
+    {
+        public Student(string name, List<string> comments, List<DateTime> datesActivities)
+        {
+            this.Name = name;
+            this.Comments = comments;
+            this.DatesActivities = datesActivities;
+        }
+
+        public string Name { get; set; }
+
+        public List<string> Comments { get; set; }
+
+        public List<DateTime> DatesActivities { get; set; }
+    }
+}
