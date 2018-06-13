aeecd623cf7d2456cf8fc7ae3d9e69c43e86e978
Kalina M <milanova1203@gmail.com>
2018-06-13 17:13:46 +0300

    Add files via upload

---
+++ Dictionaries- Lambda Expressions and LINQ/Exercises/07.PopulationCounter/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.PopulationCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> population = new Dictionary<string, Dictionary<string, int>>();
+            var cityAndPopulation = new Dictionary<string, int>();
+            List<string> input = Console.ReadLine().Split("|").ToList();
+
+            while (!input[0].Equals("report"))
+            {
+                string country = input[1];
+                string city = input[0];
+                int populationn = int.Parse(input[2]);
+
+
+                if (!population.ContainsKey(country))
+                {
+                    cityAndPopulation = new Dictionary<string, int>();
+                    cityAndPopulation[city] = populationn;
+                    population[country] = cityAndPopulation;
+                    
+                }
+                else
+                {
+                    cityAndPopulation[city] = populationn;
+                    population[country] = cityAndPopulation;
+                }
+
+                input = Console.ReadLine().Split("|").ToList();
+            }
+
+            foreach (var item in population)
+            {
+                int totalPopulation = 0;
+                foreach (var i in item.Value)
+                {
+                    totalPopulation += i.Value;                    
+                }
+         
+                Console.Write(item.Key);
+                Console.WriteLine($" (totalpopulation: {totalPopulation})");
+                foreach (var i in item.Value)
+                {
+                    Console.WriteLine("=>" + string.Join(" ", i.Key) + ":" + i.Value);
+                }
+            }
+        }
+    }
+    
+}
---
+++ Dictionaries- Lambda Expressions and LINQ/Exercises/11.DragonArmy/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+        var dragon = new Dictionary<string, SortedDictionary<string, List<double>>>();
+
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            var currentRow = Console.ReadLine().Split().ToList();
+
+            string type = currentRow[0];
+            string name = currentRow[1];
+
+            if (currentRow[2] == "null")
+            {
+                currentRow[2] = "45";
+            }
+            if (currentRow[3] == "null")
+            {
+                currentRow[3] = "250";
+            }
+            if (currentRow[4] == "null")
+            {
+                currentRow[4] = "10";
+            }
+            int damage = int.Parse(currentRow[2]);
+            int health = int.Parse(currentRow[3]);
+            int armor = int.Parse(currentRow[4]);
+
+            if (!dragon.ContainsKey(type))
+            {
+                dragon[type] = new SortedDictionary<string, List<double>>();
+            }
+            if (!dragon[type].ContainsKey(name))
+            {
+                dragon[type][name] = new List<double>();
+                dragon[type][name].Add(0);
+                dragon[type][name].Add(0);
+                dragon[type][name].Add(0);
+            }
+            dragon[type][name][0] = damage;
+            dragon[type][name][1] = health;
+            dragon[type][name][2] = armor;
+        }
+
+        foreach (var dragonData in dragon)
+        {
+            double averageDamage = dragonData.Value.Select(x => x.Value[0]).Average();
+            double averageHealths = dragonData.Value.Select(x => x.Value[1]).Average();
+            double averageArmor = dragonData.Value.Select(x => x.Value[2]).Average();
+
+            Console.WriteLine($"{dragonData.Key}::({averageDamage:f2}/{averageHealths:f2}/{averageArmor:f2})");
+            foreach (var currentDragon in dragonData.Value)
+            {
+                Console.WriteLine("-{0} -> damage: {1}, health: {2}, armor: {3}",
+                    currentDragon.Key,
+                    currentDragon.Value[0],
+                    currentDragon.Value[1],
+                    currentDragon.Value[2]
+                );
+            }
+        }
+    }
+}
