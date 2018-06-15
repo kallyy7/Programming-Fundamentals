ee8fb94f094ecb8593776c85a8c6fefc729aaeb1
Kalina Milanova <milanova1203@gmail.com>
2018-06-16 00:32:50 +0300

    Add files via upload

---
+++ Old Exams/Programming Fundamentals Retake Exam - 25 April 2018/02.MemoryView/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02MemoryView
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string result = null;
+          
+            List<string> names = new List<string>();
+
+            while (!input.Equals("Visual Studio crash"))
+            {
+                result += input + " ";
+                input = Console.ReadLine();
+            }
+
+            string[] token = result.Split();
+
+            for (int i = 0; i < token.Length - 5; i++)
+            {
+                bool startString = (token[i] == "32656") && (token[i + 1] == "19759") && (token[i + 2] == "32763") && (token[i + 3] == "0") && (token[i + 5] == "0");
+
+                if (startString)
+                {
+                    string word = null;
+                    int wordLength = int.Parse(token[i + 4]);
+
+                    for (int j = i + 6; j <= (i + 6) + wordLength; j++)
+                    {
+                        word += (char)(int.Parse(token[j]));
+                    }
+                    names.Add(word);
+                }
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, names));
+        }
+    }
+}
