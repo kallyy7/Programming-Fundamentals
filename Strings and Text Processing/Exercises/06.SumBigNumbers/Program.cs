413fb6999259e203614ee3925c5d7ea3b593107c
Kalina Milanova <milanova1203@gmail.com>
2018-06-15 21:05:59 +0300

    Add files via upload

---
+++ Strings and Text Processing/Exercises/06.SumBigNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace _06.SumBigNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstNumber = Console.ReadLine();
+            string secondNumber = Console.ReadLine();
+
+            int maxLen = Math.Max(firstNumber.Length, secondNumber.Length);
+
+            firstNumber = firstNumber.PadLeft(maxLen + 1, '0');
+            secondNumber = secondNumber.PadLeft(maxLen + 1, '0');
+
+            int[] firstDigits = firstNumber.Select(x => int.Parse(x.ToString())).ToArray();
+            int[] secondDigits = secondNumber.Select(x => int.Parse(x.ToString())).ToArray();
+            int[] sum = new int[firstNumber.Length];
+
+            int currentSum = 0;
+
+            for (int i = sum.Length - 1; i >= 0; i--)
+            {
+                int total = firstDigits[i] + secondDigits[i] + currentSum;
+                sum[i] = total % 10;
+
+                currentSum = (total > 9) ?  1 : 0;
+               
+            }
+
+            string result = string.Join(string.Empty, sum.SkipWhile(x => x == 0));
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Strings and Text Processing/Exercises/09.MelrahShake/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+namespace _09.MelrahShake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = Console.ReadLine();
+
+            StringBuilder messages = new StringBuilder();
+
+            if (pattern.Length == 0)
+            {
+                Console.WriteLine("No shake");
+                Console.WriteLine(input);
+                return;
+            }
+
+            while (true)
+            {
+                int firstOccureence = input.IndexOf(pattern);
+                int lastOccureence = input.LastIndexOf(pattern);
+
+                if (firstOccureence > -1 && lastOccureence > -1 && pattern.Length > 0)
+                {
+                    input = input.Remove(lastOccureence, pattern.Length);
+                    input = input.Remove(firstOccureence, pattern.Length);
+                    messages.AppendLine("Shaked it");
+
+                    pattern = pattern.Remove(pattern.Length / 2, 1);
+
+                }
+                else
+                {
+                    messages.AppendLine("No shake");
+                    break;
+                }
+
+            }
+
+            Console.Write(messages.ToString());
+            Console.WriteLine(input);
+        }
+    }
+}
