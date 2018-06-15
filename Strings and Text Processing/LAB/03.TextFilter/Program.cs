b076f0f462771649fcdfd86fc85e4e33dcd2668c
Kalina M <milanova1203@gmail.com>
2018-06-15 20:05:45 +0300

    Add files via upload

---
+++ Strings and Text Processing/LAB/03.TextFilter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.TextFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(", ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string input = Console.ReadLine();
+
+            foreach (var word in bannedWords)
+            {
+                if (input.Contains(word))
+                {
+                    input = input.Replace(word, new string('*', word.Length));
+                }
+            }
+
+            Console.WriteLine(input);
+        }
+    }
+}
---
+++ Strings and Text Processing/LAB/04.Polindromes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Polindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ,!?.".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).ToArray();
+            List<string> result = new List<string>();
+
+            foreach (var word in input)
+            {
+
+                if (word[0].Equals(word[word.Length - 1]))
+                {
+                    result.Add(word);
+                }
+            }
+
+            result.Sort();
+            Console.WriteLine(string.Join(", ", result));
+
+        }
+    }
+}
