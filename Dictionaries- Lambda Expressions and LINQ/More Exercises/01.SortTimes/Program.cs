cf9f7354411c5834309fb1c3cceb5539c3f35a12
Kalina M <milanova1203@gmail.com>
2018-06-15 20:12:09 +0300

    Add files via upload

---
+++ Dictionaries- Lambda Expressions and LINQ/More Exercises/01.SortTimes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.SortTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> time = Console.ReadLine().Split().ToList();
+
+            time.Sort();
+           
+            Console.WriteLine(string.Join(", ", time));           
+        }
+    }
+}
