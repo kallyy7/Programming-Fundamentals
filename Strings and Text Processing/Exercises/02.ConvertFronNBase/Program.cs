75007bc974973d3992c9b0dcb31b4535daf3830c
Kalina M <milanova1203@gmail.com>
2018-06-15 20:19:36 +0300

    Add files via upload

---
+++ Strings and Text Processing/Exercises/02.ConvertFronNBase/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Numerics;
+
+namespace _02.ConvertFronNBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            int @base = int.Parse(input[0]);
+            string number = input[1];
+
+            BigInteger sum = 0;
+            int pow = 0;
+
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                char curr = number[i];
+                int currentNumber = int.Parse(curr.ToString());
+
+                BigInteger currentSum = currentNumber * BigInteger.Pow(@base, pow);
+                sum += currentSum;
+                pow++;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Strings and Text Processing/Exercises/04.CharacterMultiplier/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.CharacterMultiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+            int sum = 0;
+            char[] first = input[0].ToCharArray();
+            char[] second = input[1].ToCharArray();
+
+            for (int i = 0; i < Math.Max(first.Length, second.Length); i++)
+            {
+                if (first.Length < i + 1)
+                {
+                    sum += second[i];
+                }
+                else if (second.Length < i + 1)
+                {
+                    sum += first[i];
+                }
+                else
+                {
+                    sum += first[i] * second[i];
+                }
+
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
