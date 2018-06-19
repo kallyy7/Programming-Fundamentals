e463c5583b9282178167328b5dfdd539a45c76bf
Kalina Milanova <milanova1203@gmail.com>
2018-06-19 21:07:34 +0300

    Add files via upload

---
+++ Regular Expressions-REGEX/Exercises/06.ValidUsernames/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _06.ValidUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine()
+                .Split(new char[] { '\\', '/', '(', ')', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Trim())
+                .ToArray();
+
+            List<string> validUsernames = new List<string>();
+
+            string pattern = @"^[A-Za-z][A-Za-z\d_]{2,24}$";
+            var reg = new Regex(pattern);
+
+            for (int i = 0; i < usernames.Length; i++)
+            {
+                if (reg.IsMatch(usernames[i]))
+                {
+                    validUsernames.Add(usernames[i]);
+                }
+            }
+
+            var maxLenght = 0;
+            var startIndex = 0;
+            for (int i = 0; i < validUsernames.Count - 1; i++)
+            {
+                if (validUsernames[i].Length + validUsernames[i + 1].Length > maxLenght)
+                {
+                    maxLenght = validUsernames[i].Length + validUsernames[i + 1].Length;
+                    startIndex = i;
+                }
+            }
+
+            Console.WriteLine(validUsernames[startIndex].Trim());
+            Console.WriteLine(validUsernames[startIndex + 1].Trim());
+        }
+    }
+}
---
+++ Regular Expressions-REGEX/Exercises/08.UseYourChainsBuddy/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _08.UseYourChainsBuddy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string htmlText = Console.ReadLine();
+            string pattern = @"<p>(.+?)<\/p>";
+
+            string currentMach = string.Empty;
+
+            foreach (Match match in Regex.Matches(htmlText, pattern))
+            {
+                currentMach += match.Groups[1].Value;
+            }
+
+            currentMach = Regex.Replace(currentMach, @"[^a-z\d]", " ");
+            currentMach = Regex.Replace(currentMach, @"\s+|\n+", " ");
+            StringBuilder result = new StringBuilder(currentMach.Length);
+
+            foreach (var ch in currentMach)
+            {
+                if (ch >= 'a' && ch <= 'm')
+                {
+                    result.Append((char)(ch + 13));
+                }
+                else if (ch >= 'n' && ch <= 'z')
+                {
+                    result.Append((char)(ch - 13));
+                }
+                else
+                {
+                    result.Append(ch);
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
