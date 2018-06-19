3acd5d4f0aa8fc52e9787f64abdecfe4ed4f04b3
Kalina Milanova <milanova1203@gmail.com>
2018-06-19 09:50:45 +0300

    Add files via upload

---
+++ Regular Expressions-REGEX/Lab/05.MatchNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _05.MatchNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(^|(?<=\s))-?\d+(\.\d+)?($|(?=\s))";
+
+            string input = Console.ReadLine();
+            var numbers = Regex.Matches(input, pattern);
+
+            foreach (Match n in numbers)
+            {
+                Console.Write(n.Value + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
