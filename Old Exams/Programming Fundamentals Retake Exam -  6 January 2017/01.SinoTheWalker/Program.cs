c5fcb747b86e7240ba059684505033ce7310d9e7
Kalina Milanova <milanova1203@gmail.com>
2018-06-20 17:59:23 +0300

    Add files via upload

---
+++ Old Exams/Programming Fundamentals Retake Exam -  6 January 2017/01.SinoTheWalker/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01.SinoTheWalker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time = DateTime.Parse(Console.ReadLine());
+            long numberOfSteps = long.Parse(Console.ReadLine());
+            long timeInSeconds = long.Parse(Console.ReadLine());
+            long totalSeconds = numberOfSteps * timeInSeconds;
+
+            DateTime timeArrival = time.AddSeconds(totalSeconds);
+
+            Console.WriteLine($"Time Arrival: {timeArrival:HH:mm:ss}");
+        }
+    }
+}
