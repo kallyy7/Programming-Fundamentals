b4971dd429cb7c8f046978a69c7901ceef009791
Kalina Milanova <milanova1203@gmail.com>
2018-06-20 20:24:11 +0300

    Add files via upload

---
+++ Old Exams/Programming Fundamentals Sample Exam I - October 2016/01.SoftuniCoffee/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace _01.SoftuniCoffee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOrders = int.Parse(Console.ReadLine());
+            decimal total = 0;
+
+            for (int i = 0; i < countOrders; i++)
+            {
+                decimal priceCapsule = decimal.Parse(Console.ReadLine());
+                DateTime orderDate = DateTime.ParseExact(Console.ReadLine(), "d/M/yyyy", CultureInfo.InvariantCulture);
+                int daysInMonth = DateTime.DaysInMonth(orderDate.Year, orderDate.Month);
+                long capsulesCount = long.Parse(Console.ReadLine());
+                decimal currentPrice = (daysInMonth * capsulesCount) * priceCapsule;
+                total += currentPrice;
+
+                Console.WriteLine($"The price for the coffee is: ${currentPrice:f2}");
+            }
+
+            Console.WriteLine($"Total: ${total:f2}");
+        }
+    }
+}
