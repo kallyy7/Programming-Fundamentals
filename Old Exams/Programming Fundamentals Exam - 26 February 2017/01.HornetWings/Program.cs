79ac1d1b611420a90691aea73015d29f29f2bf15
Kalina Milanova <milanova1203@gmail.com>
2018-06-20 17:30:22 +0300

    Add files via upload

---
+++ Old Exams/Programming Fundamentals Exam - 26 February 2017/01.HornetWings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01.HornetWings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long wingFlapsN = long.Parse(Console.ReadLine());
+            double distanceM = double.Parse(Console.ReadLine());
+            long enduranceP = long.Parse(Console.ReadLine());
+
+            double metersTraveled = (wingFlapsN / 1000) * distanceM;
+            double flaps = wingFlapsN / 100;
+            double secondsPassed = ((wingFlapsN / enduranceP) * 5) + flaps;
+
+            Console.WriteLine($"{metersTraveled:f2} m.");
+            Console.WriteLine($"{secondsPassed} s.");
+
+        }
+    }
+}
