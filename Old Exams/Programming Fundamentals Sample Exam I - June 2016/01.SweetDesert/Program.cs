5a9b0fb43ed748530a8ba7ae7ac51f851b1be43a
Kalina Milanova <milanova1203@gmail.com>
2018-06-20 21:45:52 +0300

    Add files via upload

---
+++ Old Exams/Programming Fundamentals Sample Exam I - June 2016/01.SweetDesert/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01.SweetDesert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal amountOfCash = decimal.Parse(Console.ReadLine());
+            int numberOfGuests = int.Parse(Console.ReadLine());
+            decimal bananasPrice = decimal.Parse(Console.ReadLine());
+            decimal eggsPrice = decimal.Parse(Console.ReadLine());
+            decimal berriestPrice = decimal.Parse(Console.ReadLine());
+
+            decimal portions = Math.Ceiling(numberOfGuests / 6.0m);
+            decimal totalPrice = (portions * (2 * bananasPrice)) + (portions * (4 * eggsPrice)) + (portions * (0.2m * berriestPrice));
+
+            if (amountOfCash >= totalPrice)
+            {
+                Console.WriteLine($"Ivancho has enough money - it would cost {totalPrice:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivancho will have to withdraw money - he will need {(totalPrice - amountOfCash):f2}lv more.");
+            }
+        }
+    }
+}
