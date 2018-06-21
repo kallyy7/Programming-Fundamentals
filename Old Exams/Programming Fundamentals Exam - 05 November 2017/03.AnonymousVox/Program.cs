1522bb4bc28051e81d1fd9f3516b2992022b3a4b
Kalina Milanova <milanova1203@gmail.com>
2018-06-21 17:23:09 +0300

    Add files via upload

---
+++ Old Exams/Programming Fundamentals Exam - 05 November 2017/03.AnonymousVox/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _03.AnonymousVox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string encodedText = Console.ReadLine();
+            string[] placeholders = Console.ReadLine().Split("{ }".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string pattern = @"([A-Za-z]+)(.+)(\1)";
+            MatchCollection matches = Regex.Matches(encodedText, pattern);
+            string replaced = null;
+            int count = 0;
+
+            foreach (Match match in matches)
+            {
+                string curr = match.Groups[2].Value;
+                string currentPlaceholder = placeholders[count];
+
+                string decoded = match.Groups[1] + placeholders[count] + match.Groups[3];
+                encodedText = encodedText.Replace(match.Groups[0].Value, decoded);
+
+                replaced = encodedText.Replace(curr, currentPlaceholder);
+                count++;
+            }
+            Console.WriteLine(replaced);
+        }
+    }
+}
